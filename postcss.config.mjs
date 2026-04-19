0d6338756d2d417bd04df7443e0abb4f65f4720c
seyit ali <seyitali5742@gmail.com>
2026-04-19 23:02:14 +0200

    fix: install tailwindcss postcss plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // Değişen satır burası
     autoprefixer: {},
   },
 };
